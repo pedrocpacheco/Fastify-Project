fb12a585b823fcfaf8c159d96d2d82a8e1a10312
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-28 16:59:20 -0300

    config: eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.get("/hello", () => {
-  return "Pedro"
+app.get('/hello', () => {
+  return 'Pedro'
 })
 
-app.listen({
-  port: 3333
-}).then(() => console.log("HTTP Server Running!"));
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('HTTP Server Running!'))
